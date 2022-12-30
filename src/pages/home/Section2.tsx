042508199d19cf0b4b286ecedac75e0439384949
Howie <howie12207@gmail.com>
2022-12-30 15:25:48 +0800

    chore: remove content

--- src/pages/home/Section2.tsx
+++ src/pages/home/Section2.tsx
@@ -6,7 +6,7 @@ const Section2 = () => {
     return (
         <section className="form-1-section py-10 px-2 sm:py-20">
             <h2 className="mb-8 text-center text-2xl font-black text-blue-700 sm:text-3xl">
-                輕鬆開戶立享3筆定期定額終身0手續費
+                輕鬆開戶立享3筆定期定額0手續費
             </h2>
             <div className="relative flex items-start justify-center">
                 <img
